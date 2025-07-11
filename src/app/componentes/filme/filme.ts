7e151384243b66f98f6586fb0d2813dca470bb5f
Francisco <francisco.tonini@aluno.senac.br>
2025-07-10 21:38:04 -0300

    agora tem favorito no filme

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  alternarFavorito(){
+    this.filme1.favorito = !this.filme1.favorito;
+  }
+
+  filme1 = {
+    nome: "Os Esparalhoes",
+    imagem: "/1.jpg",
+    sinopse: "''Melhor filme de ação ja feito'' Senhor Cinema ヽ( ´ー`)ノ",
+    favorito: false
+  }
+  filme2 = {
+    nome: "ScottPilgrim Contra o Mundo",
+    imagem: "/2.jpg",
+    sinopse: "Tem que bate em ex qua ai não é violencia domestica",
+    favorito: false
+  }
+  filme3 = {
+    nome: "Clube da Luta",
+    imagem: "/3.jpg",
+    sinopse: "Tem que bater em voce mesmo de vez em quando",
+    favorito: false
+  }
 }
